411b08ce1ef21de67dc42e1aefd5319c7a7be4b0
Ruben Dario Jordan Saenz <jordanrubendario@gmail.com>
2020-12-07 12:49:42 -0500

    Semodifican los archivos para que funcionen con flask

--- templatemo_466_cafe_house/app.py
+++ templatemo_466_cafe_house/app.py
@@ -1,9 +1,13 @@
 from flask import Flask as fl
+from flask import render_template
+import os
+
 
 app = fl(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world"
-if __name__ == '__main__':
+    return render_template("UpdateUser.html")
+
+if __name__ == "__main__":
     app.run(debug=True)
