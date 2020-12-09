ab766f0cda52d11849ff1af9ce5183d9f8c5b1fc
cgacharna <cgacharna@uninorte.edu.co>
2020-12-08 19:49:06 -0500

    Se agregaron las rutas de todos los elementos

--- templatemo_466_cafe_house/app.py
+++ templatemo_466_cafe_house/app.py
@@ -9,9 +9,52 @@ app = fl(__name__)
 def hello():
     return render_template("login.html")
 
+@app.route("/RecoverPassword")
+def RecoverPassword():
+    return render_template("RecoverPassword.html")
+
+
+
+
+@app.route("/HomeAdmin")
+def HomeAdmin():
+    return render_template("HomeAdmin.html")
+
+@app.route("/HomeUser")
+def HomeUser():
+    return render_template("HomeUser.html")
+
+@app.route("/ManageUser")
+def ManageUser():
+    return render_template("ManageUser.html")
+
+@app.route("/NewUser")
+def NewUser():
+    return render_template("NewUser.html")
+
+@app.route("/SearchUser")
+def SearchUser():
+    return render_template("SearchUser.html")
+
+
+   
+
+@app.route("/NewProduct")  
+def NewProduct():
+    return render_template("NewProduct.html")
+
 @app.route("/UpdateProduct")
 def UpdateProduct():
     return render_template("UpdateProduct.html")
+
+@app.route("/UpdateProductUser")
+def UpdateProductUser():
+    return render_template("UpdateProductUser.html") 
+
+@app.route("/SearchProduct")
+def SearchProduct():
+    return render_template("SearchProduct.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
